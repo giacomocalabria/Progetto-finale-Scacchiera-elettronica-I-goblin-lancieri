b2990a42c7c0964d890d916ef260ca95009ae1bb
giacomocalabria <95050451+giacomocalabria@users.noreply.github.com>
2021-12-24 15:51:28 +0100

    scansione delle diagonali di queen

--- src/queen.cpp
+++ src/queen.cpp
@@ -33,25 +33,35 @@ vector<position> queen::get_possible_positions(){
             possible_pos.push_back(dest);
     }
 
-    //diagonale principale
-    if(pos.col-pos.row > 0){
+    
+    if(pos.col >= pos.row ){
+        //diagonale secondaria
         for(int i = 0; i < 7 - (pos.col - pos.row - 1); i++){ 
-            dest = position((pos.col-pos.row) + i, i);
+            dest = position(i, i + (pos.col - pos.row));
             if(!(dest == pos))
                 possible_pos.push_back(dest);
         }
     }
     else{
+        //diagonale secondaria
         for(int i = 0; i < 7 - (pos.row - pos.col - 1); i++){ 
-            dest = position((pos.row - pos.col) + i, i);
+            dest = position(i + (pos.row - pos.col), i);
             if(!(dest == pos))
                 possible_pos.push_back(dest);
         }
     }
-
-    //diagonale secondaria
-    for(int i = 0; i < 7 - (pos.row-pos.col); i++){
-        dest = position(7+(pos.col-pos.row), i);
+    if(pos.row + pos.col < 7){
+        //diagonale primaria
+        for(int i = 0; i < (pos.row + pos.col); i++){
+        dest = position((pos.col + pos.row) - i, i);
+        if(!(dest == pos))
+            possible_pos.push_back(dest);
+        }
+    }
+    else{
+        //diagonale secondaria
+        for(int i = (pos.row + pos.col) - 7; i < 7; i++){
+        dest = position(7 + pos.row - i , i);
         if(!(dest == pos))
             possible_pos.push_back(dest);
     }
