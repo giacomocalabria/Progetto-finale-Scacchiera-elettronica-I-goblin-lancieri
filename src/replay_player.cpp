f68c920387ff18f658e52d85b6f4689dbabba63e
giacomocalabria <95050451+giacomocalabria@users.noreply.github.com>
2021-12-28 15:05:55 +0100

    replay player di due tipi: file e video

--- src/replay_player.cpp
+++
@@ -1,7 +0,0 @@
-#include "replay_player.h"
-
-void replay_player::turn(std::string& mossa){
-    position from = position(mossa.substr(0,2));
-    position to = position(mossa.substr(2,2));
-    b->move_piece(from, to);
-}
---
+++ src/video_replay_player.cpp
@@ -0,0 +1,40 @@
+#include "video_replay_player.h"
+
+using namespace std;
+
+void video_replay_player::turn(ifstream& elenco_mosse){
+    string mossa;
+    bool success = false;
+    while(!success){
+        getline(elenco_mosse, mossa);
+        if(move(mossa)){
+            success = true;
+        }
+    }
+}
+
+bool video_replay_player::move(std::string& mossa){
+    if(mossa.length() != 5){
+        cout << "Mossa "<< mossa << " non valida! " << endl;
+        return false;
+    }
+    position from = position(mossa.substr(0,2));
+    position to = position(mossa.substr(2,2));
+    if(is_valid_position_8(from) && is_valid_position_8(to)){
+        cout << "Mossa "<< mossa << " non valida! La posizione non è nella scacchiera" << endl;
+        return false;
+    }
+    if(b->get_board_piece(from)->get_player() != get_player_number()){
+        cout << "Mossa "<< mossa << " non valida! Non puoi muovere una pedina avversaria" << endl;
+        return false;
+    }
+    if(b->get_board_piece(to)->get_player() == get_player_number()){
+        cout << "Mossa "<< mossa << " non valida! Non puoi andare su una tua pedina" << endl;
+        return false;
+    }
+
+    if(! b->move_piece(from, to)){
+        cout << "Mossa "<< mossa << " non valida! Non puoi fare quella mossa" << endl;
+        return false;
+    }
+}
